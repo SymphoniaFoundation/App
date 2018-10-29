7c00bb85014cdf055f26dd9ee83d5f43ed4f4e37
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-29 11:37:09 +0000

    Deploy SymphoniaFoundation/SymphoniaApp to github.com/SymphoniaFoundation/SymphoniaApp.git:master

--- src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'sym-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-}
